84f1cda196583d9684c9ea7ea695f27ccb2b589f
Owner <Owner@LAPTOP-57CST924.nyc.rr.com>
2022-07-03 23:44:20 -0400

    shear something  first time in github.

---
+++ student/src/test/java/student/Teacher.java
@@ -0,0 +1,8 @@
+package student;
+
+public class Teacher {
+	public static void main(String[] args) {
+		System.out.println(student.name);
+	}
+
+}
---
+++ student/src/test/java/student/student.java
@@ -0,0 +1,15 @@
+package student;
+
+public class student {
+	int number =7;
+	static String name="murad";
+	public static void main(String[] args) {
+		double result=10.99;
+		
+	}
+	public void result() {
+		System.out.println(number);
+	}
+	
+
+}
